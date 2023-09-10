69879d589a8d85b1ede9d4e947134270f6d46c0e
Lane Bretschneider <lanebret85@gmail.com>
2023-09-10 15:51:26 -0700

    feat: add all objects from tests to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+it = Department.create!(name: "IT", floor: "Basement")
+
+christina_aguilera = it.employees.create!(name: "Christina Aguilera", level: 2)
+bobby_flay = it.employees.create!(name: "Bobby Flay", level: 3)
+
+broken_printer = Ticket.create!(subject: "Printers Broken", age: 5)
+dropped_database = Ticket.create!(subject: "Database Dropped", age: 1)
+coffee_filters = Ticket.create!(subject: "Out of Coffee Filters", age: 3)
+
+EmployeeTicket.create!(employee_id: christina_aguilera.id, ticket_id: broken_printer.id)
+EmployeeTicket.create!(employee_id: bobby_flay.id, ticket_id: broken_printer.id)
+
+EmployeeTicket.create!(employee_id: christina_aguilera.id, ticket_id: dropped_database.id)
+EmployeeTicket.create!(employee_id: bobby_flay.id, ticket_id: dropped_database.id)
+
+EmployeeTicket.create!(employee_id: bobby_flay.id, ticket_id: coffee_filters.id)
