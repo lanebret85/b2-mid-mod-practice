156b797d28053903bf9b9231be2444435d6ada05
Lane Bretschneider <lanebret85@gmail.com>
2023-09-10 15:58:04 -0700

    feat: add relationships to tickets through employee tickets and sort_tickets_by_age and oldest_ticket model methods

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,16 @@
 class Employee < ApplicationRecord
   belongs_to :department
+  has_many :employee_tickets
+  has_many :tickets, through: :employee_tickets
 
   validates :name, presence: true
   validates :level, presence: true
+
+  def sort_tickets_by_age
+    tickets.order(age: :desc)
+  end
+
+  def oldest_ticket
+    sort_tickets_by_age.first
+  end
 end
