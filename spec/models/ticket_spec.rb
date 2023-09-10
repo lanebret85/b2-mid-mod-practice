084d8206de6a7f9f8d4bb15f4e5f44e9ed8b3c4f
Lane Bretschneider <lanebret85@gmail.com>
2023-09-10 11:28:29 -0700

    test: add tests for relationships and validations for all model classes

---
+++ spec/models/ticket_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe Ticket, type: :model do
+  describe "relationships" do
+    it { should have_many(:employee_tickets) }
+    it { should have_many(:employees).through(:employee_tickets) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:subject) }
+    it { should validate_presence_of(:age) }
+  end
+end
