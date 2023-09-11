c088782d58b882902460746da102152731d4ea1f
Lane Bretschneider <lanebret85@gmail.com>
2023-09-10 18:08:11 -0700

    feat: add shared_ticket_employees and unique_employees methods

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -13,4 +13,25 @@ class Employee < ApplicationRecord
   def oldest_ticket
     sort_tickets_by_age.first
   end
+
+  
+  def shared_ticket_employees(ticket)
+    other_employees = []
+    employees = Employee.joins(:tickets).where(tickets: ticket)
+    employees.each do |employee|
+      other_employees << employee
+    end
+    other_employees.delete(self)
+    other_employees
+  end
+
+  def unique_employees
+    employees = []
+    self.tickets.each do |ticket|
+      shared_ticket_employees(ticket).each do |employee|
+        employees << employee
+      end
+    end
+    employees.uniq
+  end
 end
