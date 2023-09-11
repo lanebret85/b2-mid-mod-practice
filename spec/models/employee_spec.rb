612febac7f50e44deeb3bf9bbc81735c34670e8f
Lane Bretschneider <lanebret85@gmail.com>
2023-09-10 18:07:20 -0700

    test: add tests for shared_ticket_employees and unique_employees model methods

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -15,14 +15,19 @@ RSpec.describe Employee, type: :model do
   before :each do
     @it = Department.create!(name: "IT", floor: "Basement")
 
+    @christina_aguilera = @it.employees.create!(name: "Christina Aguilera", level: 2)
     @bobby_flay = @it.employees.create!(name: "Bobby Flay", level: 3)
+    @jimmy_fallon = @it.employees.create!(name: "Jimmy Fallon", level: 4)
     
     @broken_printer = Ticket.create!(subject: "Printers Broken", age: 5)
     @dropped_database = Ticket.create!(subject: "Database Dropped", age: 1)
     @coffee_filters = Ticket.create!(subject: "Out of Coffee Filters", age: 3)
     
+    EmployeeTicket.create!(employee_id: @christina_aguilera.id, ticket_id: @broken_printer.id)
     EmployeeTicket.create!(employee_id: @bobby_flay.id, ticket_id: @broken_printer.id)
+    EmployeeTicket.create!(employee_id: @christina_aguilera.id, ticket_id: @dropped_database.id)
     EmployeeTicket.create!(employee_id: @bobby_flay.id, ticket_id: @dropped_database.id)
+    EmployeeTicket.create!(employee_id: @jimmy_fallon.id, ticket_id: @dropped_database.id)
     EmployeeTicket.create!(employee_id: @bobby_flay.id, ticket_id: @coffee_filters.id)
   end
 
@@ -39,6 +44,22 @@ RSpec.describe Employee, type: :model do
       query = @bobby_flay.oldest_ticket
 
       expect(query).to eq(@broken_printer)
+    end
+  end
+
+  describe "#shared_ticket_employees" do
+    it "can return a unique list of all other employees who share a single ticket" do
+      query = @bobby_flay.shared_ticket_employees(@dropped_database)
+
+      expect(query).to eq([@christina_aguilera, @jimmy_fallon])
+    end
+  end
+
+  describe "#unique employees" do
+    it "can return a unique list of employees who share any tickets with a given employees" do
+      query = @bobby_flay.unique_employees
+
+      expect(query).to eq([@christina_aguilera, @jimmy_fallon])
     end
   end
 end
